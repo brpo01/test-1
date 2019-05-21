56316fb95fc45c99e353d58b84b174fa1fee1d48
Praise <opraise139@gmail.com>
2019-05-21 16:21:12 -0700

    second commit

---
+++ test1.py
@@ -0,0 +1,37 @@
+# a program that prints your name 100 times
+name = ' Bola-Rotimi Praise'
+your_name = (name * 100)
+
+for item in your_name:
+    print(name)
+
+# a program that prints integers 1-20
+i = 1
+while i < 20:
+    sq = i * i
+    i += 1
+    print(f'{i} --- {sq}')
+
+
+# star
+i = 0
+while i < 5:
+    i += 1
+    print('*' * i)
+
+# square
+numbers = [14, 2, 2, 14]
+for count in numbers:
+    if count == 14:
+        print(f'x' * count)
+    else:
+        print(f'x' + f'            ' + f'x')
+
+
+
+
+
+
+
+
+
