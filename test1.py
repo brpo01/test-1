4272ea8fc7ec115d58f8ec0d7e8a6356be78aabd
Praise <opraise139@gmail.com>
2019-05-22 13:08:39 -0700

    third commit

--- test1.py
+++ test1.py
@@ -3,21 +3,18 @@ name = ' Bola-Rotimi Praise'
 your_name = (name * 100)
 
 for item in your_name:
-    print(name)
+    print(f'my name is {name}')
 
 # a program that prints integers 1-20
-i = 1
-while i < 20:
-    sq = i * i
-    i += 1
-    print(f'{i} --- {sq}')
+for x in range(21):
+    square = x * x
+    print(f'{x} --- {square}')
 
 
-# star
-i = 0
-while i < 5:
-    i += 1
-    print('*' * i)
+# triangle
+triangle = [1, 2, 3, 4, 5, 6 ]
+for num in triangle:
+    print(num * '*')
 
 # square
 numbers = [14, 2, 2, 14]
@@ -26,6 +23,52 @@ for count in numbers:
         print(f'x' * count)
     else:
         print(f'x' + f'            ' + f'x')
+
+# 'A' shape
+for lines in range(4):
+    if lines == 0:
+        print('    *  ')
+    elif lines == 1:
+        print('  *   *')
+    elif lines == 2:
+        print(' *  *  *')
+    else:
+        print('*       *')
+
+#factorial
+def factorial(n):
+   if n == 1:
+       return n
+   else:
+       return n*factorial(n-1)
+
+num = int(input("Enter a number: "))
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   print(f'The factorial of {num} is {factorial(num)}')
+
+#recursive fxn
+def calculate(number):
+    if number == 1:
+        return f'f(n) = 3'
+    else:
+        a = 3 ** num
+        return (f"f(n) = {a}")
+num = float(input('Enter a number: '))
+print(calculate(num))
+
+
+
+
+
+
+
+
+
+
 
 
 
